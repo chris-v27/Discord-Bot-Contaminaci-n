350395a5415d5f0e9b3a2b2377292dac355c2b3f
chris-v27 <vanegaschristian.0422@gmail.com>
2024-11-22 16:00:32 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 from bot_logic import *
 from discord.ext import commands
+import random
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -11,8 +12,21 @@ bot = commands.Bot(command_prefix='/', intents=intents)
 async def on_ready():
     print("Se inicio el bot")
 
+residuos= {'Residuos orgánicos': ["Cáscaras de frutas y verduras", "Restos de comida", "Cáscaras de huevo", "Hojas secas",],
+              'Residuos reciclables': ["Bolsas plásticas", "Tapas", "Periódicos", "revistas", "hojas de papel", "Cajas y cartón", "Envases de vidrio", "Jarrones", "Utensilios de cocina", "Latas",       ],
+              'Residuos no reciclables': ["Pañales", "Esponjas de cocina", "Envolturas de caramelos"]}
+
 @bot.command()
-async def hola(ctx):
-    await ctx.send("Hola, como estas")
+async def info(ctx):
+    await ctx.send("Escribe -/residuos- para ver la lista de materiales disponibles a desechar",
+                   "\nEscribe -/desechar- y escribe un número correspondiente, en que lugar se puede desechar el residuo",
+                   "\nEscribe -/consejos- para ver una lista de consejos que te ayudaran a mejorar el planeta")
+
+@bot.command()
+async def residuos(ctx):
+    lst = ''
+    for residuo in range(random.shuffle(residuos.values())):
+        lst += f'{residuo}\n'
+    await ctx.send(lst)
 
 bot.run("Token")
